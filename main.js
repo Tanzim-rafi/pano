1b6450457807f238b266be85a6ec56f1aa1ff0b3
Tanzim khan Rafi <116541680+Tanzim-rafi@users.noreply.github.com>
2023-04-08 21:28:26 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const pan=document.querySelector('.pan');
 const img='pan.jpeg';
-const panoroma=new Panolens Image
+const panoroma=new Panolens.ImagePanorama(img);
+
+const viewer=new PANOLENS.Viewer({
+ container:pan;
+autoRotate:true;
+autoRotateSpeed:.5
+});
+viewer.add(panorama);
